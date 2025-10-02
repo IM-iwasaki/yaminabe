7497cff8c1f24f0d06f1cf73cdb150e1f9fa0545
Umalus <tenderhorse67@gmail.com>
2025-10-02 15:17:09 +0900

    Commit

--- Assets/_TeamMember/Tahara/Script/Player.cs
+++ Assets/_TeamMember/Tahara/Script/Player.cs
@@ -1,41 +1,49 @@
 using Mirror;
 using UnityEngine;
 
-public class Player : NetworkBehaviour
-{
+public class Player : NetworkBehaviour {
     [SerializeField]
     private GameObject bullet = null;
     [SerializeField]
     private Transform fire = null;
+    [SerializeField]
+    private GameObject playerCamera = null;
     private void Awake() {
         var net = GetComponent<NetworkTransformHybrid>();
         net.syncDirection = SyncDirection.ServerToClient;
     }
-    private void Update() {
-        if (isLocalPlayer) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                CmdShootBullet();
-            }
+    private void Start() {
+        if (!isLocalPlayer) {
+            playerCamera.SetActive(false);
+        }
+        else {
+            playerCamera.SetActive(true);
         }
     }
 
     private void FixedUpdate() {
         if (isLocalPlayer) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                CmdShootBullet();
+            }
+
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
             CmdPlayerMove(x, z);
         }
+
+
     }
     [Command]
-    void CmdPlayerMove(float _x,float _z) {
+    void CmdPlayerMove(float _x, float _z) {
         Vector3 v = new Vector3(_x, 0, _z) * 5;
         GetComponent<Rigidbody>().AddForce(v);
     }
 
     [Command]
     void CmdShootBullet() {
-        GameObject obj = Instantiate(bullet,fire.position,fire.rotation);
-        NetworkServer.Spawn(obj,connectionToClient);
+        GameObject obj = Instantiate(bullet, fire.position, fire.rotation);
+        NetworkServer.Spawn(obj, connectionToClient);
     }
 }
