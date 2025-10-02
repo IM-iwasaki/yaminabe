8a14503e9aadbb34a5c98608d28ba11ae4207369
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2025-10-02 14:52:34 +0900

    1452

--- Assets/_TeamMember/Kira/Scripts/Player/PlayerController.cs
+++ Assets/_TeamMember/Kira/Scripts/Player/PlayerController.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour {
+    //移動の入力方向
+    private Vector2 moveInput;
+    private Vector3 moveDirection;
+
+    //RigidBodyをキャッシュ
+    private Rigidbody rb;
+
+    //仮。
+    float moveSpeed = 5.0f;
+    float turnSpeed = 8.0f;
+
+    //移動入力を受け付けるコンテキスト
+    public void OnMove(InputAction.CallbackContext context) {
+        moveInput = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Update() {
+        //カメラの向きを取得
+        Transform cameraTransform = Camera.main.transform;
+        //進行方向のベクトルを取得
+        Vector3 forward = cameraTransform.forward;
+        forward.y = 0f;
+        forward.Normalize();
+        //右方向のベクトルを取得
+        Vector3 right = cameraTransform.right;
+        right.y = 0f;
+        right.Normalize();
+        //2つのベクトルを合成
+        moveDirection = (forward * moveInput.y + right * moveInput.x);
+
+        // カメラの向いている方向をプレイヤーの正面に
+        Vector3 aimForward = forward; // 水平面だけを考慮
+        if (aimForward != Vector3.zero) {
+            Quaternion targetRot = Quaternion.LookRotation(aimForward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, turnSpeed * Time.deltaTime);
+        }
+
+        // 移動方向にキャラクターを向ける
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, moveDirection.z * moveSpeed);
     }
 }
