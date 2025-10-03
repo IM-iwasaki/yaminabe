5495f3f7237e8fe42941cd875473c4ed1583015f
Yu-Matuo <mcd2476068@stu.o-hara.ac.jp>
2025-10-03 10:13:47 +0900

    ゲームマネージャー

--- Assets/_TeamMember/Matuo/Script/Manager/GameManager.cs
+++ Assets/_TeamMember/Matuo/Script/Manager/GameManager.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+/// <summary>
+/// ゲーム全体の開始と終了を管理するマネージャー
+/// SystemObjectを継承してるお
+/// </summary>
+public class GameManager : SystemObject<GameManager> {
+    private bool isGameRunning = false;
+
+    /// <summary>
+    /// 初期化処理
+    /// SystemManagerから呼ばれる
+    /// </summary>
+    public override void Initialize() {
+        base.Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// <summary>
+    /// ゲームを開始する
+    /// </summary>
+    public void StartGame() {
+        if (isGameRunning) return;
+        isGameRunning = true;
+        // マップとかプレイヤー生成するならここでヨロ
+
+    }
+
+    /// <summary>
+    /// ゲームを終了する
+    /// </summary>
+    public void EndGame() {
+        if (!isGameRunning) return;
+        isGameRunning = false;
+        // リザルト画面とかはここで
+
+    }
+
+    /// <summary>
+    /// ゲームが進行中かどうかを返す
+    /// </summary>
+    public bool IsGameRunning() {
+        return isGameRunning;
     }
 }
