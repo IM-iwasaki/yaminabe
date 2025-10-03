142d61ac3ddbcfc2f63560ab69614ce516426a9c
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2025-10-03 16:40:12 +0900

    1640

--- Assets/_TeamMember/Kira/Scripts/CommandMessanger.cs
+++ Assets/_TeamMember/Kira/Scripts/CommandMessanger.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CommandMessanger : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class CommandMessanger : MonoBehaviour {
+    //ˆÚ“®“ü—Í—v‹‚ğCommand‚É“n‚·
+    //void Cmd_Move() {
+    //}
+    
 }
