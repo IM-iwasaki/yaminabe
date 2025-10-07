eb258e66a670ef6164b3372939cba44cbaaa7d3a
Umalus <tenderhorse67@gmail.com>
2025-10-07 09:59:54 +0900

    connect Player
    接続したプレイヤーの人数をサーバーで管理しているが、その人数が接続切断のタイミングで数値が変わるように調整

--- Assets/_TeamMember/Tahara/Script/CustomNetworkManager.cs
+++ Assets/_TeamMember/Tahara/Script/CustomNetworkManager.cs
@@ -5,17 +5,27 @@ public class CustomNetworkManager : NetworkManager
 {
     [SerializeField]
     private ServerManager serverManager = null;
-
+    /// <summary>
+    /// オーバーライドしたOnServerAddPlayer
+    /// サーバーに参加したことを伝える(具体的にはconnectPlayerに参加したタイミングでAddする)
+    /// </summary>
+    /// <param name="_conn"></param>
     public override void OnServerAddPlayer(NetworkConnectionToClient _conn) {
         //base.OnServerAddPlayer(_conn);
         GameObject player = Instantiate(playerPrefab);
         NetworkServer.AddPlayerForConnection(_conn, player);
 
-        serverManager.connectPlayer.Add(player.GetComponent<NetworkIdentity>());
+        serverManager.connectPlayer.Add(_conn.identity);
     }
 
+    /// <summary>
+    /// オーバーライドしたOnServerDisconnect
+    /// クライアントが抜けたタイミングでconnectPlayerからRemoveする
+    /// </summary>
+    /// <param name="_conn"></param>
     public override void OnServerDisconnect(NetworkConnectionToClient _conn) {
+        serverManager.connectPlayer.Remove(_conn.identity);
         base.OnServerDisconnect(_conn);
-        serverManager.connectPlayer.RemoveAt(serverManager.connectPlayer.Count - 1);
+        
     }
 }
