c9b34ef98537b9254299a57de39809c9362ead1c
Umalus <tenderhorse67@gmail.com>
2025-10-08 15:33:44 +0900

    HP Bar
    HP bar仮作成
    クライアント側が同期してない

--- Assets/_TeamMember/Tahara/Script/DemoPlayer.cs
+++ Assets/_TeamMember/Tahara/Script/DemoPlayer.cs
@@ -1,6 +1,7 @@
 using Mirror;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 全部デバッグ用クラス
@@ -14,10 +15,18 @@ public class DemoPlayer : NetworkBehaviour {
     [SerializeField]
     private Camera playerCamera = null;
     public int TeamID = -1;
-    [SyncVar(hook = nameof(Changed))]
-    float speed = 0;
+    
+    [SerializeField, SyncVar(hook = nameof(ChangedHP))]
+    private int HP = 100;
     [SerializeField]
-    private TextMeshProUGUI text = null;
+    TextMeshProUGUI text = null;
+    [SerializeField]
+    private Canvas playerUI = null;
+    [SerializeField]
+    Slider slider = null;
+    private float timer = 0;
+    private float interval = 1;
+
     private void Awake() {
         var net = GetComponent<NetworkTransformHybrid>();
         net.syncDirection = SyncDirection.ServerToClient;
@@ -29,6 +38,7 @@ public class DemoPlayer : NetworkBehaviour {
         }
         else {
             playerCamera.gameObject.SetActive(false);
+            playerUI.gameObject.SetActive(false);
         }
     }
 
@@ -41,6 +51,15 @@ public class DemoPlayer : NetworkBehaviour {
             playerCamera.gameObject.SetActive(false);
         }
     }
+    private void Update() {
+        timer += Time.deltaTime;
+        
+        if(HP > 0 && timer > interval) {
+            HP -= 1;
+            timer = 0;
+        }
+        
+    }
 
     private void FixedUpdate() {
         if (isLocalPlayer) {
@@ -52,7 +71,7 @@ public class DemoPlayer : NetworkBehaviour {
             float z = Input.GetAxis("Vertical");
 
             CmdPlayerMove(x, z);
-            speed = GetComponent<Rigidbody>().velocity.x;
+            
         }
         
 
@@ -68,9 +87,11 @@ public class DemoPlayer : NetworkBehaviour {
         GameObject obj = Instantiate(bullet, fire.position, fire.rotation);
         NetworkServer.Spawn(obj, connectionToClient);
     }
-
-    private void Changed(float _old, float _new) {
-        speed = _new;
-        text.text = (speed).ToString();
+    private void ChangedHP(int _old,int _new) {
+        if (isLocalPlayer) {
+            text.text = _new.ToString();
+            slider.value = HP;
+        }
+       
     }
 }
