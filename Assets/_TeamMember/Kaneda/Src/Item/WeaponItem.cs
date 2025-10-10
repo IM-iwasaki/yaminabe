98e31e0826a3cb23d6601c688cd3c27659beac47
kuroneko1318 <nekkokuro5@gmail.com>
2025-10-10 11:38:01 +0900

    武器取得処理追加
    武器を取得時にデータを置き換え

--- Assets/_TeamMember/Kaneda/Src/Item/WeaponItem.cs
+++ Assets/_TeamMember/Kaneda/Src/Item/WeaponItem.cs
@@ -16,14 +16,14 @@ public class WeaponItem : ItemBase {
     /// </summary>
     public override void Use(GameObject player) {
         //  プレイヤー処理(プレイヤーが出来次第追加)
-        NetworkWeapon playerWeapon = player.GetComponent<NetworkWeapon>();
-        if (playerWeapon != null) {
+        NetworkWeapon playerWeaponData = player.GetComponent<NetworkWeapon>();
+        if (playerWeaponData != null) {
             Debug.LogWarning("プレイヤーの中にNetworkWeaponが見つかりませんでした");
             return;
         }
 
         //  持っている武器データをプレイヤーに受け渡す
-
+        playerWeaponData.SetWeaponData(weaponData);
 
         // 使用後にアイテムを削除
         Destroy(gameObject);
