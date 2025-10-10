e959ed236f79c8aada3c341c2a30c1615a67169d
kuroneko1318 <nekkokuro5@gmail.com>
2025-10-10 11:31:08 +0900

    武器チェンジの受け皿を作成

--- Assets/_TeamMember/Kaneda/Src/Item/WeaponItem.cs
+++ Assets/_TeamMember/Kaneda/Src/Item/WeaponItem.cs
@@ -15,14 +15,23 @@ public enum WeaponType {
 public class WeaponItem : ItemBase {
     [Header("武器タイプ")]
     public WeaponType weaponType;
-    //  武器で取得する能力値などを追加する
-
+    //  武器データ保存
+    [Header("作製したデータをこの中に入れる")]
+    [SerializeField]
+    private WeaponData weaponData = null;
 
     /// <summary>
     /// 使用処理
     /// </summary>
     public override void Use(GameObject player) {
         //  プレイヤー処理(プレイヤーが出来次第追加)
+        NetworkWeapon playerWeapon = player.GetComponent<NetworkWeapon>();
+        if (playerWeapon != null) {
+            Debug.LogWarning("プレイヤーの中にNetworkWeaponが見つかりませんでした");
+            return;
+        }
+
+        //  持っている武器データをプレイヤーに受け渡す
 
 
         // 使用後にアイテムを削除
