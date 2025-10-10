a6d858bf501cccd940c6bd1b3660d7788c8afd73
Yu-Matuo <mcd2476068@stu.o-hara.ac.jp>
2025-10-10 13:45:40 +0900

    セーブ機能

--- Assets/_TeamMember/Matuo/Script/Gacha/GachaItem.cs
+++ Assets/_TeamMember/Matuo/Script/Gacha/GachaItem.cs
@@ -9,7 +9,7 @@ public class GachaItem {
     public string itemName;
 
     [Header("確率設定")]
-    [Range(0, 100)]
+    [Range(0, 10)]
     public int rate = 1;
 
     [Header("景品プレハブ(仕様によっては使わないかも)")]
