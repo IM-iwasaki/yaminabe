d0a86c3647284e71c194e9c901cfb20735ce3166
Yu-Matuo <mcd2476068@stu.o-hara.ac.jp>
2025-10-14 12:39:58 +0900

    commit

--- Assets/_TeamMember/Matuo/Script/Player/PlayerData.cs
+++ Assets/_TeamMember/Matuo/Script/Player/PlayerData.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 [System.Serializable]
 public class PlayerData {
     public int currentMoney;                       // プレイヤーの所持金
-    public List<PlayerItemStatus> items = new();  // 取得済みアイテムリスト
+    public List<PlayerItemStatus> items = new();   // 取得済みアイテムリスト
 }
