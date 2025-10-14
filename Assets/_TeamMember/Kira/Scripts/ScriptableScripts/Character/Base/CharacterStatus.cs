5443df23eea08a5c3b9855b4a4a07c075a13f887
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2025-10-14 10:39:40 +0900

    1039

--- Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Character/Base/CharacterStatus.cs
+++ Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Character/Base/CharacterStatus.cs
@@ -8,7 +8,10 @@ public abstract class CharacterStatus : ScriptableObject {
     public StatusBase BaseStatus;
     [Tooltip("キャラクター名(職業名)を記入してください。\n日本語でも構いません。")]
     public string DisplayName;
-    public SkillBase[] Skills; // ここに職業固有スキルを登録する。
+    [Tooltip("このキャラクターが使うスキルを割り当ててください。\n(複数アタッチできますが0番目のものしか使えません。)")]
+    public SkillBase[] Skills;
+    [Tooltip("このキャラクターが発動可能なパッシブを割り当ててください。\n(複数アタッチできますが0番目のものしか使えません。)")]
+    public PassiveBase[] Passives;
 
     public virtual int MaxHP => BaseStatus.MaxHP;
     public virtual int Attack => BaseStatus.Attack;
