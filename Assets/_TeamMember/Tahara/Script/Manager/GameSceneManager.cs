e24d9b1f08afa49fdfcd4fbae9322e3f1b996063
Umalus <tenderhorse67@gmail.com>
2025-10-15 10:32:36 +0900

    Scene Manager
    シーンのロード、アンロードを持ったマネージャーを作成

--- Assets/_TeamMember/Tahara/Script/Manager/GameSceneManager.cs
+++ Assets/_TeamMember/Tahara/Script/Manager/GameSceneManager.cs
@@ -4,19 +4,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameSceneManager :NetworkSystemObject<GameSceneManager>
-{
-    [SerializeField]
+public class GameSceneManager :NetworkSystemObject<GameSceneManager> {
+    private string oldSceneName = null;
+    
+    [SerializeField, Header("読み込むロビーシーンの名前")]
+    private string lobbySceneName = "LobbyScene";
+    [SerializeField,Header("読み込むゲームシーンの名前")]
     private string gameSceneName = "GameScene";
+    
 
-    public void LoadGameScene() {
-        if (!isServer) return;
-        SceneManager.LoadSceneAsync(gameSceneName, LoadSceneMode.Additive);
+    
+    [Server]
+    public void LoadScene(string _sceneName) {
+        //ロードする前(現在)のシーンの名前をキャッシュ
+        oldSceneName = SceneManager.GetActiveScene().name;
+        //重ねるシーンをロード
+        SceneManager.LoadSceneAsync(_sceneName, LoadSceneMode.Additive);
+    }
+
+    [Server]
+    public void UnLoadScene(string _sceneName) {
+        //ロード時にキャッシュしたシーンを解放
+        SceneManager.UnloadSceneAsync(_sceneName);
+    }
+
+    /// <summary>
+    /// 全てのクライアントに対してゲームシーンを重ね、前のシーンを解放する
+    /// ホストが特定のタイミングで呼び出す
+    /// </summary>
+    [ClientRpc]
+    public void LoadGameSceneForAll() {
+
+        LoadScene(gameSceneName);
+        UnLoadScene(oldSceneName);
     }
 
     [ClientRpc]
-    public void LoadGameSceneForAll() {
-        GameSceneManager.Instance.LoadGameScene();
+    public void LoadLobbySceneForAll() {
+        LoadScene(lobbySceneName);
+        UnLoadScene(oldSceneName);
     }
 
 
