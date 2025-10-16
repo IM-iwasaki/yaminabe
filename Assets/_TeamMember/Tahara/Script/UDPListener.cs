275943ce7f93f31397df982645bb721dff716db4
Umalus <tenderhorse67@gmail.com>
2025-10-16 15:28:02 +0900

    Title Manager
    タイトルからロビーへIPアドレスの入力なしでクライアントで移動することが確認できた。しかし同PC内なので別PCだとどうなるかはわからない

---
+++ Assets/_TeamMember/Tahara/Script/UDPListener.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Concurrent;
+using System;
+
+public class UDPListener : MonoBehaviour {
+    ConcurrentQueue<UdpMessage> messageQueue = new ConcurrentQueue<UdpMessage>();
+    [System.Serializable]
+    public class UdpMessage {
+        public string ip;
+        public int port;
+        public string gameName;
+        public string hostName;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        Task.Run(ReceiveMessageFromBroadcaster);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        while (messageQueue.TryDequeue(out UdpMessage msg)) {
+            TitleManager.instance.ipAddress = msg.ip;
+        }
+    }
+
+    public async Task ReceiveMessageFromBroadcaster() {
+        UdpClient udpClient = new UdpClient(7777);
+        try {
+            while (true) {
+                UdpReceiveResult result = await udpClient.ReceiveAsync();
+                string json = Encoding.UTF8.GetString(result.Buffer);
+                UdpMessage message = JsonUtility.FromJson<UdpMessage>(json);
+
+                messageQueue.Enqueue(message);
+
+            }
+
+        }
+        catch (Exception error) {
+            Debug.LogError(error.Message);
+        }
+        finally {
+            udpClient.Close();
+        }
+
+    }
+}
