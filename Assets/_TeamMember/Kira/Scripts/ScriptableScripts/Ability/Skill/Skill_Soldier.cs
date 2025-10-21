f2b3b68d7158ccf42a125b2887db640d1bf7443e
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2025-10-21 11:39:34 +0900

    スキル関連処理の追加。
    スキルを使えるようにしました。

--- Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Ability/Skill/Skill_Soldier.cs
+++ Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Ability/Skill/Skill_Soldier.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Character/Skill/Soldier_積極攻勢")]
 public class Skill_Soldier : SkillBase {
+
+    //
+    //  スキル名：積極攻勢
+    //  タイプ　：自己強化型
+    //  効果    ：10秒間、攻撃力が30％上昇する。
+    //　CT      ：20秒
+    //
+
     public override void Activate(CharacterBase user) {
-        
+        //攻撃力上昇開始
+        user.AttackBuff(1.3f,10.0f);
+        //デバッグログを出す
+        Debug.Log("スキルを使用しました。");
     }
 }
