31e3ff4e929a18aeadb05c89eeceb192c5f318c5
marsh <f.koji5654@gmail.com>
2025-10-28 14:20:24 +0900

    地雷作成中

---
+++ Assets/_TeamMember/Furuya/Src/Weapon/Trap/TrapBase.cs
@@ -0,0 +1,39 @@
+using Mirror;
+using System.Collections;
+using UnityEngine;
+
+public struct TrapInitData {
+    public int teamID;
+    public float activationDelay;
+    public bool activationOnce;
+    public EffectType activationEffect;
+    public float duration;
+}
+
+public abstract class TrapBase : NetworkBehaviour {
+    protected int ownerTeamID;
+    protected bool isActivated;
+    protected bool hasTriggered;
+
+    [Server]
+    public virtual void Init(TrapInitData data) {
+        ownerTeamID = data.teamID;
+        StartCoroutine(ActivationDelayRoutine(data.activationDelay));
+    }
+
+    [Server]
+    protected IEnumerator ActivationDelayRoutine(float delay) {
+        yield return new WaitForSeconds(delay);
+        isActivated = true;
+    }
+
+    [ClientRpc(includeOwner = true)]
+    protected void RpcPlayEffect(Vector3 pos, EffectType effectType) {
+        var fx = WeaponPoolRegistry.Instance.GetHitEffect(effectType);
+        if (fx != null) {
+            var instance = EffectPoolManager.Instance.GetFromPool(fx, pos, Quaternion.identity);
+            instance.SetActive(true);
+            EffectPoolManager.Instance.ReturnToPool(instance, 1.5f);
+        }
+    }
+}
