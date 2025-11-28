b0691d08398afbf2efa8834c9683781808146420
PurplePerry <mamorumamo10292005@au.com>
2025-11-28 14:59:56 +0900

    ファイター作った

--- Assets/_TeamMember/Iwasaki/Res/Iwa_Character/Passive/Passive_Fighter.cs
+++ Assets/_TeamMember/Iwasaki/Res/Iwa_Character/Passive/Passive_Fighter.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Character/Passive/Gunman_逃げ足")]
-public class Passive_Gunman : PassiveBase {
+[CreateAssetMenu(menuName = "Character/Passive/Fighter_根性")]
+public class Passive_Fighter : PassiveBase {
 
     //
-    // パッシブ名　：逃げ足
+    // パッシブ名　：根性
     // タイプ      ：HP発動型
-    // 効果        ：自身の残り体力が20％以下になった時、10秒間の30％のスピードバフが入る
-    //               この効果は一度発動すると35秒間は発動しない。
-    //
-
-   
+    // 効果        ：HPが一定以下には受けるダメージを軽減。
+    //クールタイム ：30
 
     public override void PassiveReflection(CharacterBase user) {
         //発動中でなかったらクールタイムを計測
@@ -21,14 +18,22 @@ public class Passive_Gunman : PassiveBase {
                 isPassiveActive = true;
                 //クールタイム計測をリセット
                 coolTime = 0;
+                // 
+               
             }
+            // 10秒で元に戻るはず
+            else if (coolTime >=Cooldown/3) {
+                user.DamageRatio = 100;
+            }
+
         }
 
+
         //発動中にHPが条件を満たしたら発動。
-        if (isPassiveActive && user.HP <= user.maxHP / 5) {
-            user.MoveSpeedBuff(0.3f, 10.0f);
-            //発動状態を解除
-            isPassiveActive = false;
+        if (isPassiveActive && user.HP <= user.maxHP / 4) {
+            isPassiveActive=false;
+            user.DamageRatio = 50;
         }
+       
     }
 }
