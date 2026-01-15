dcbbaecdcdb4a577d7af7ceec73d137462caedb1
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2026-01-15 11:19:40 +0900

    クエーサーのパッシブ調整。スクリプトのコメント追加など。

--- Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Ability/Passive/Passive_Chaser.cs
+++ Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Ability/Passive/Passive_Chaser.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Character/Skill/Chaser_マジックチェイン")]
@@ -7,11 +8,16 @@ public class Passive_Chaser : PassiveBase {
     // パッシブ名　：マジックチェイン
     // タイプ      ：攻撃持続強化型
     // 効果        ：攻撃を行うたびにスキルのCTがごくわずかに短縮される。
-    //               攻撃を行った回数に応じて効果が上昇する。(最大10回まで)
+    //               攻撃を行った回数に応じて効果が上昇する。(最大50回まで)
     //
 
     //攻撃のインターバルを計測
     private float intervalTime = 0;
+
+    //CT短縮の効果の大きさ
+    private readonly float CTAcceleration = 0.01f;
+    //パッシブ蓄積数の最大数
+    private readonly int passiveMaxChains = 50;
 
     public override void PassiveSetting() {
         passiveChains = 0;
@@ -24,14 +30,14 @@ public class Passive_Chaser : PassiveBase {
         //攻撃した瞬間にインターバルが経過していたら
         if (user.parameter.isAttackPressed && intervalTime >= user.weaponController_main.weaponData.cooldown) {
             //チェインは最大50個まで、最大でなければチェインを蓄積
-            if(passiveChains < 50){
+            if(passiveChains < passiveMaxChains){
                 passiveChains++;
             }
             //インターバルリセット
             intervalTime = 0;
 
             //チェインの多さに応じてスキルCTを短縮
-            user.parameter.skillAfterTime += (0.01f * passiveChains);
+            user.parameter.skillAfterTime += (CTAcceleration * passiveChains);
             //スキルCTが最大だったら補正
             float skillCooldown = user.GetComponent<GeneralCharacter>().parameter.equippedSkills[0].cooldown;
             if(user.parameter.skillAfterTime >= skillCooldown) {
--- Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Ability/Skill/Skill_Chaser.cs
+++ Assets/_TeamMember/Kira/Scripts/ScriptableScripts/Ability/Skill/Skill_Chaser.cs
@@ -12,48 +12,52 @@ public class Skill_Chaser : SkillBase {
     //
 
     //使用後の経過時間を計測
-    private float UseTime = 0;
+    private float useTime = 0;
     //スキルの効果時間を定義
-    private const float EffectTime = 6.0f;
+    private readonly float effectTime = 6.0f;
     //追加攻撃のインターバルを計測
-    private float IntervalTime = 0;
+    private float intervalTime = 0;
+    //追加攻撃の発生ディレイ
+    private float intervalDelay;
 
     public override void Activate(CharacterBase user) {
         //効果発動
         isSkillUse = true;
         //時間計測をリセット
-        UseTime = 0;
+        useTime = 0;
     }
 
     public override void SkillEffectUpdate(CharacterBase user) {
+        intervalDelay = user.weaponController_main.weaponData.cooldown / 2;
+
         //使用中か確認、効果中は時間を計測
         if(isSkillUse) {
-            UseTime += Time.deltaTime;
-            IntervalTime += Time.deltaTime;
+            useTime += Time.deltaTime;
+            intervalTime += Time.deltaTime;
 
             //効果時間を過ぎたら効果を終了
-            if (UseTime >= EffectTime) {
-                isSkillUse = false;
-            }
+            if (useTime >= effectTime) isSkillUse = false;
 
             //攻撃が入力された中かつインターバルが経過していたら
-            if(user.parameter.isAttackPressed && IntervalTime >= user.weaponController_main.weaponData.cooldown / 2) {
+            if(user.parameter.isAttackPressed && intervalTime >= intervalDelay) {
                 //インターバルをリセット
-                IntervalTime = 0;
+                intervalTime = 0;
                 //若干の遅延を入れて追加攻撃発動
-                RequestExtraAttackWithDelay(user.weaponController_main.weaponData.cooldown/2, user);
+                RequestExtraAttackWithDelay(intervalDelay, user);
             }
         }
     }
 
+    //遅延をかけて追加攻撃開始の合図を送る
     public void RequestExtraAttackWithDelay(float delay,CharacterBase user){
         user.StartCoroutine(RequestExtraAttackRoutine(delay,user));
     }
 
+    //遅延処理後に追加攻撃開始の合図を送る
     private IEnumerator RequestExtraAttackRoutine(float delay,CharacterBase user){
         yield return new WaitForSeconds(delay);
 
-        // ここで実行
+        // ここで追加攻撃を実行
         Vector3 shootDir = user.parameter.GetShootDirection();
         user.weaponController_main.CmdRequestExtraAttack(shootDir);        
     }
