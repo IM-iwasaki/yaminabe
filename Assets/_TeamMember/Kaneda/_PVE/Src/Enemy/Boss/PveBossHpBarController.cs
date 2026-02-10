d900bf4b9b3c770bea3643124b095164bf15ed67
kuroneko1318 <nekkokuro5@gmail.com>
2026-02-10 14:26:01 +0900

    ボスのHPBarを追加

---
+++ Assets/_TeamMember/Kaneda/_PVE/Src/Enemy/Boss/PveBossHpBarController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PveBossHpBarController : MonoBehaviour
+{
+
+    [Header("参照")]
+    [SerializeField] private EnemyParameter enemyParameter;
+
+    [Header("HP表示")]
+    [SerializeField] private Slider hpSlider;
+
+    [Header("名前表示")]
+    [SerializeField] private TextMeshProUGUI nameText;
+
+    private EnemyStatusBase enemyStatus;
+
+    private void Start() {
+        if (enemyParameter != null && hpSlider != null) {
+            hpSlider.maxValue = enemyParameter.HP;
+            hpSlider.value = enemyParameter.HP;
+        }
+
+        enemyStatus = GetComponent<EnemyStatusBase>();
+        if (nameText == null) return;
+        nameText.SetText(enemyStatus.statusData.name);
+    }
+
+    /// <summary>
+    /// HP更新（EnemyStatusBaseから呼ぶ）
+    /// </summary>
+    public void UpdateHP(int currentHP) {
+        if (hpSlider != null) {
+            hpSlider.value = currentHP;
+        }
+    }
+
+
+}
