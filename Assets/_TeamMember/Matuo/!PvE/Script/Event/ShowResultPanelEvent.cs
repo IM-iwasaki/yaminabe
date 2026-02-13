c74cac454d2bad78778535239d706231e64c9e5f
Yu-Matuo <mcd2476068@stu.o-hara.ac.jp>
2026-02-13 14:19:29 +0900

    PVE用リザルト表示とスポーン関係修正

--- Assets/_TeamMember/Matuo/!PvE/Script/Event/ShowResultPanelEvent.cs
+++ Assets/_TeamMember/Matuo/!PvE/Script/Event/ShowResultPanelEvent.cs
@@ -11,6 +11,14 @@ public class ShowResultPanelEvent : PVEStageEvent {
 
     public override void Execute() {
         if (resultPanel == null) return;
+        bool isHost = NetworkServer.active;
+        // ホストがオプションを開いていたら閉じる
+        if (isHost) {
+            OptionMenu optionMenu = FindObjectOfType<OptionMenu>();
+            if (optionMenu != null && optionMenu.isOpen) {
+                optionMenu.ToggleMenu(); // 閉じる
+            }
+        }
 
         resultPanel.SetActive(true);
 
