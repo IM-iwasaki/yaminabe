3f9bd9f774afb2d73a936a085ccab2a8a47cdd19
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2026-03-25 14:15:43 +0900

    Null直した

--- Assets/_TeamMember/Kira/Scripts/_CharacterClass/CharacterInput.cs
+++ Assets/_TeamMember/Kira/Scripts/_CharacterClass/CharacterInput.cs
@@ -77,7 +77,6 @@ public class CharacterInput : NetworkBehaviour {
         SkillTriggered = false;
         InteractTriggered = false;
         isJumpPressed = false;
-        core.parameter.hitDamage = false;
 
         if (core != null) core.parameter.attackTrigger = false;
     }
