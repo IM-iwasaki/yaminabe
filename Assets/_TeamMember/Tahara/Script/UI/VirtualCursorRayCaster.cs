586261a7c9007c79d695c66dbc2553e2cc107d51
Umalus <tenderhorse67@gmail.com>
2026-05-28 16:11:14 +0900

    仮想マウス進捗

--- Assets/_TeamMember/Tahara/Script/UI/VirtualCursorRayCaster.cs
+++ Assets/_TeamMember/Tahara/Script/UI/VirtualCursorRayCaster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class VirtualCursorRayCaster : MonoBehaviour
 {
@@ -12,12 +13,14 @@ public class VirtualCursorRayCaster : MonoBehaviour
     List<RaycastResult> currentHits = new List<RaycastResult>();
     List<GameObject> lastHits = new List<GameObject>();
 
+    GameObject pressedObject;
     // Update is called once per frame
     void Update()
     {
         pointerData = new PointerEventData(eventSystem);
         pointerData.position = cursor.position;
 
+        //Raycast
         currentHits.Clear();
         eventSystem.RaycastAll(pointerData, currentHits);
 
@@ -25,15 +28,26 @@ public class VirtualCursorRayCaster : MonoBehaviour
         foreach (var hit in currentHits)
             currentObjects.Add(hit.gameObject);
 
+        //PointerEnter
         foreach (var obj in currentObjects){
             if (!lastHits.Contains(obj))
             ExecuteEvents.Execute(obj.gameObject, pointerData, ExecuteEvents.pointerEnterHandler);
         }
-
+        //PointerExit
         foreach(var lastObj in lastHits)
         {
             if (!currentObjects.Contains(lastObj)){
                 ExecuteEvents.Execute(lastObj.gameObject, pointerData, ExecuteEvents.pointerExitHandler);
+            }
+        }
+
+        //PointerDown
+        if(Gamepad.current != null && Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            if(currentObjects.Count > 0)
+            {
+                pressedObject = currentObjects[0];
+                ExecuteEvents.Execute(pressedObject, pointerData, ExecuteEvents.pointerDownHandler);
             }
         }
 
